1564e5a2d550097344a272fa9f351099b729d27e
Matheus Ferreira <matheus.44300@gmail.com>
2022-03-01 14:24:18 -0300

    Removed ADS on Open App

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
-import 'package:flutter_video_cut/app/shared/ads/app_open_ad_manager.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'app/app.dart';
@@ -18,8 +17,6 @@ void main() async {
   } else {
     FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
   }
-
-  await AppOpenAdManager().loadAd();
 
   runApp(const App());
 }
