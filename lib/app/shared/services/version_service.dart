a286326a4272d5355ee5a2fab1948d3a70917f0b
Matheus Ferreira <matheus.44300@gmail.com>
2022-03-10 00:06:39 -0300

    Created VersionService

---
+++ lib/app/shared/services/version_service.dart
@@ -0,0 +1,29 @@
+import 'package:new_version/new_version.dart';
+
+abstract class IVersionService {
+  Future<bool> updateAvailable();
+  Future<void> launchAppStore();
+}
+
+class VersionService implements IVersionService {
+  final _newVersion = NewVersion();
+
+  @override
+  Future<void> launchAppStore() async {
+    final version = await _newVersion.getVersionStatus();
+    if (version == null) {
+      return;
+    }
+    await _newVersion.launchAppStore(version!.appStoreLink);
+  }
+
+  @override
+  Future<bool> updateAvailable() async {
+    final version = await _newVersion.getVersionStatus();
+    if (version == null) {
+      return false;
+    }
+
+    return version.canUpdate;
+  }
+}
