7627abe9cecb6b7b6eab82abb2e896c858b89317
Matheus Ferreira <matheus.44300@gmail.com>
2022-05-12 12:15:56 -0300

    Created video service

---
+++ lib/app/interfaces/ivideo_service.dart
@@ -0,0 +1,3 @@
+abstract class IVideoService {
+  void cutVideo({required String url, int seconds = 20});
+}
---
+++ lib/app/services/video_service.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_video_cut/app/interfaces/ivideo_service.dart';
+
+class VideoService implements IVideoService {
+  @override
+  void cutVideo({required String url, int seconds = 20}) {
+    // TODO: implement cutVideo
+  }
+}
