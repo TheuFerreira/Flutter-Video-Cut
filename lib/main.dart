3879e88179380406174b5dc7d668f61e96d4baff
Matheus Ferreira <matheus.44300@gmail.com>
2022-05-14 16:35:53 -0300

    Starting firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 
 import 'app/app.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp();
+
+  if (kDebugMode) {
+    await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(false);
+  } else {
+    FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
+  }
+
   runApp(const App());
 }
