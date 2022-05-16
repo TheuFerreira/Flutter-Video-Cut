b24f004a9587a73e009bcd7128874014d254495d
Matheus Ferreira <matheus.44300@gmail.com>
2022-05-15 22:43:52 -0300

    Added function to Show Question Dialog

--- lib/app/interfaces/idialog_service.dart
+++ lib/app/interfaces/idialog_service.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/material.dart';
+
 abstract class IDialogService {
   void showMessage(String message);
   void showMessageError(String message);
+  Future<bool> showQuestionDialog(
+      BuildContext context, String title, String description);
 }
--- lib/app/services/dialog_service.dart
+++ lib/app/services/dialog_service.dart
@@ -1,5 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_video_cut/app/dialogs/question_dialog.dart';
 import 'package:flutter_video_cut/app/interfaces/idialog_service.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DialogService implements IDialogService {
   @override
@@ -13,5 +16,26 @@ class DialogService implements IDialogService {
       msg: message,
       toastLength: Toast.LENGTH_LONG,
     );
+  }
+
+  @override
+  Future<bool> showQuestionDialog(
+    BuildContext context,
+    String title,
+    String description,
+  ) async {
+    final delete = await showDialog<bool>(
+      barrierDismissible: false,
+      context: context,
+      builder: (ctx) {
+        return QuestionDialog(
+          icon: FontAwesomeIcons.triangleExclamation,
+          title: title,
+          description: description,
+        );
+      },
+    );
+
+    return delete == true;
   }
 }
