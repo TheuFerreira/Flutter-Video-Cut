1710ef8514e751b964f7e693db70359164ba5798
Matheus Ferreira <matheus.44300@gmail.com>
2022-08-30 23:25:53 -0300

    Created logic to clear cache

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,52 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_background_service/flutter_background_service.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter_video_cut/app_module.dart';
+import 'package:flutter_video_cut/domain/use_cases/delete_file_from_storage_case.dart';
+import 'package:flutter_video_cut/domain/use_cases/get_files_in_cache_case.dart';
+import 'package:flutter_video_cut/infra/services/log_service_impl.dart';
+import 'package:flutter_video_cut/infra/services/path_service_impl.dart';
+import 'package:flutter_video_cut/infra/services/storage_service_impl.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 
 import 'app_widget.dart';
 
+void onStart(ServiceInstance service) async {
+  final pathService = PathServiceImpl();
+
+  final getFilesInCacheCase = GetFilesInCacheCaseImpl(pathService);
+  final paths = await getFilesInCacheCase();
+
+  for (final path in paths) {
+    final extension = pathService.getExtensionFileName(path);
+
+    if (!extension.contains('mp4')) continue;
+
+    final deleteFileFromStorageCase = DeleteFileFromStorageCaseImpl(StorageServiceImpl(), LogServiceImpl());
+    deleteFileFromStorageCase(path);
+  }
+
+  service.stopSelf();
+}
+
+bool onIosBackground(ServiceInstance service) => true;
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  final service = FlutterBackgroundService();
+  await service.configure(
+    iosConfiguration: IosConfiguration(onForeground: onStart, onBackground: onIosBackground),
+    androidConfiguration: AndroidConfiguration(
+      onStart: onStart,
+      isForegroundMode: false,
+      autoStart: true,
+      foregroundServiceNotificationTitle: 'Limpeza',
+      foregroundServiceNotificationContent: 'Limpando resíduos...',
+    ),
+  );
 
   await Firebase.initializeApp();
   await MobileAds.instance.initialize();
